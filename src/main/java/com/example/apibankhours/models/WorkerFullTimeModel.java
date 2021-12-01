dd56b25e139e20cf4897ef79fc7aaffaa3277ff1
wictor-parmenis <wictortec@gmail.com>
2021-12-01 09:20:40 -0300

    Add model of workerFullTime entity.

--- src/main/java/com/example/apibankhours/models/WorkerFullTimeModel.java
+++ src/main/java/com/example/apibankhours/models/WorkerFullTimeModel.java
@@ -1,5 +1,26 @@
 package com.example.apibankhours.models;
 
-public class WorkerFullTimeModel {
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+public class WorkerFullTimeModel extends Worker {
+
+    @Id
+    private String id;
+    private float extraHoursWorked;
+
+    @Builder
+    public WorkerFullTimeModel(float extraHoursWorked, String name, String cpf, String jobName, String email,float totalHoursWorked, String level) {
+        super(name, cpf, jobName, email,totalHoursWorked, level);
+        this.setExtraHoursWorked(extraHoursWorked);
+
+    }
 }
+
+
+
